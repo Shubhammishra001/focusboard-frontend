7a04aee736d3915754089cf251bce44aeb0c0676
Shubham Mishra <shubhammishra4620@gmail.com>
2025-07-27 01:16:16 +0530

    successfully set up the frontend with login, register, and dashboard routing.

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,6 @@
+// RegisterPage.js
+import React from 'react';
+
+export default function RegisterPage() {
+  return <h2>dash board  Page</h2>;
+}
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,6 @@
+// RegisterPage.js
+import React from 'react';
+
+export default function RegisterPage() {
+  return <h2>login  Page</h2>;
+}
